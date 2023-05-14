e1db2c7292c8452c5159f4915107314c6e1ab215
Zombi <zombeifedgaming@outlook.com>
2023-05-14 00:02:40 -0500

    update installer

--- scripts/install.bat
+++ scripts/install.bat
@@ -8,5 +8,4 @@ if exist %MY_PATH% ( echo ) else ( mkdir %MY_PATH% )
 echo f | xcopy /f /y "bin/SoupPackager%ver%.exe" "%MY_PATH%\soup_packager.exe"
 setx path "%MY_PATH%;%PATH%"
 cls
-echo Soup Has Been installed For %USERNAME%.
-call "%MY_PATH%/soup.exe"
+echo Soup Packager Has Been installed For %USERNAME%.
