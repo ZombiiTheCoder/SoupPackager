e2f33434635dd66ddc99f98110ec366b546531b8
Zombi <zombeifedgaming@outlook.com>
2023-05-14 00:10:00 -0500

    fixed install script

--- scripts/install.bat
+++ scripts/install.bat
@@ -1,11 +1,11 @@
 @echo off
 set "ver=_0.0.1a"
 if exist "bin\" ( echo ) else ( mkdir "bin\" )
-deno compile -A --allow-run --output ../bin/SoupPackager%ver%.exe main.ts
-cls
+deno compile -A --allow-run --output bin/SoupPackager%ver%.exe main.ts
+@REM cls
 set "MY_PATH=C:\Users\%USERNAME%\MuBin"
 if exist %MY_PATH% ( echo ) else ( mkdir %MY_PATH% )
 echo f | xcopy /f /y "bin/SoupPackager%ver%.exe" "%MY_PATH%\soup_packager.exe"
 setx path "%MY_PATH%;%PATH%"
-cls
+@REM cls
 echo Soup Packager Has Been installed For %USERNAME%.
